a749e0ba206270ee82907d82f7a11aa66ff37b68
fernandacastanha <fernanda.colbalchini@gmail.com>
2023-11-15 18:59:45 -0300

    Alteração no código

---
+++ index.js
@@ -0,0 +1,20 @@
+let nome = "PrincesaEscarlate";
+let xp = 8.0;
+
+if (xp < 1.0) {
+  console.log("O Herói de nome " + nome + " está no nível de Ferro");
+} else if (xp >= 1.001 && xp <= 2.0) {
+  console.log("O Herói de nome " + nome + " está no nível de Bronze");
+} else if (xp >= 2.001 && xp <= 5.0) {
+  console.log("O Herói de nome " + nome + " está no nível de Prata");
+} else if (xp >= 6.001 && xp <= 7.0) {
+  console.log("O Herói de nome " + nome + " está no nível de Ouro");
+} else if (xp >= 7.001 && xp <= 8.0) {
+  console.log("O Herói de nome " + nome + " está no nível de Platina");
+} else if (xp >= 8.001 && xp <= 9.0) {
+  console.log("O Herói de nome " + nome + " está no nível de Ascendente");
+} else if (xp >= 9.001 && xp <= 10.0) {
+  console.log("O Herói de nome " + nome + " está no nível de Imortal");
+} else if (xp >= 10.001) {
+  console.log("O Herói de nome " + nome + " está no nível de Radiante");
+}
